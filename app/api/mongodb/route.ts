2283a94ff4cb641c5f53fc68257bb5eeafbb5afa
aytuncdemr <aytunc04@hotmail.com>
2025-03-26 20:09:26 +0300

    last commit

--- app/api/mongodb/route.ts
+++ app/api/mongodb/route.ts
@@ -1,69 +1,69 @@
-import { User } from "@/interfaces/interfaces";
+import { User } from "@/interfaces/User";
 import mongodbClient from "@/utils/mongodb";
 import { ObjectId } from "mongodb";
 
 export async function GET(request: Request) {
-	const url = new URL(request.url);
-	const collectionName = url.searchParams.get("collection");
-	const query = url.searchParams.get("query");
-	const options = url.searchParams.get("options");
+    const url = new URL(request.url);
+    const collectionName = url.searchParams.get("collection");
+    const query = url.searchParams.get("query");
+    const options = url.searchParams.get("options");
 
-	if (!collectionName) {
-		return new Response(JSON.stringify("Collection name is required"), {
-			status: 400,
-		});
-	}
+    if (!collectionName) {
+        return new Response(JSON.stringify("Collection name is required"), {
+            status: 400,
+        });
+    }
 
-	const database = (await mongodbClient).db(
-		process.env.MONGODB_DB_NAME as string
-	);
+    const database = (await mongodbClient).db(
+        process.env.MONGODB_DB_NAME as string
+    );
 
-	const collection = database.collection(collectionName);
+    const collection = database.collection(collectionName);
 
-	const data = await collection
-		.find(
-			query ? JSON.parse(query) : {},
-			options
-				? { ...JSON.parse(options), projection: { password: 0 } } // password will not be sent
-				: { projection: { password: 0 } } // password will not be sent
-		)
-		.toArray();
+    const data = await collection
+        .find(
+            query ? JSON.parse(query) : {},
+            options
+                ? { ...JSON.parse(options), projection: { password: 0 } } // password will not be sent
+                : { projection: { password: 0 } } // password will not be sent
+        )
+        .toArray();
 
-	if (data) {
-		return new Response(JSON.stringify(data), { status: 200 });
-	}
+    if (data) {
+        return new Response(JSON.stringify(data), { status: 200 });
+    }
 
-	return new Response(JSON.stringify("Beklenmedik bir hata oluştu"), {
-		status: 500,
-	});
+    return new Response(JSON.stringify("Beklenmedik bir hata oluştu"), {
+        status: 500,
+    });
 }
 
 export async function POST(request: Request) {
-	try {
-		const data = (await request.json()) as User;
-		if (!data) {
-			return new Response("Invalid data", { status: 400 });
-		}
-		const database = (await mongodbClient).db(
-			process.env.MONGODB_DB_NAME as string
-		);
-		const collection = database.collection("users");
-		const { _id, ...updateData } = data;
+    try {
+        const data = (await request.json()) as User;
+        if (!data) {
+            return new Response("Invalid data", { status: 400 });
+        }
+        const database = (await mongodbClient).db(
+            process.env.MONGODB_DB_NAME as string
+        );
+        const collection = database.collection("users");
+        const { _id, ...updateData } = data;
 
-		const result = await collection.findOneAndUpdate(
-			{ _id: new ObjectId(_id) },
-			{ $set: updateData },
-			{ returnDocument: "after" } // Return updated document
-		);
-		if (result) {
-			return new Response("User updated", { status: 200 });
-		} else {
-			return new Response("User not found", { status: 404 });
-		}
-	} catch (error) {
-		return new Response(
-			JSON.stringify(error instanceof Error ? error.message : error),
-			{ status: 500 }
-		);
-	}
+        const result = await collection.findOneAndUpdate(
+            { _id: new ObjectId(_id) },
+            { $set: updateData },
+            { returnDocument: "after" } // Return updated document
+        );
+        if (result) {
+            return new Response("User updated", { status: 200 });
+        } else {
+            return new Response("User not found", { status: 404 });
+        }
+    } catch (error) {
+        return new Response(
+            JSON.stringify(error instanceof Error ? error.message : error),
+            { status: 500 }
+        );
+    }
 }
